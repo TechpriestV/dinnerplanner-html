53e48e51c7fd2af787baeae0e2db6652d7c495c8
Gustav Bergman <gustav@n180-p211.kthopen.kth.se>
2017-01-30 15:01:10 +0100

    asdasd

--- js/app.js
+++ js/app.js
@@ -3,5 +3,5 @@ $(function() {
 	var model = new DinnerModel();
 	
 	//And create the needed controllers and views
-	var exampleView = new ExampleView($("#exampleView"),model);
+	var foodGridView = new FoodGridView($("#js-recipes"),model);
 });
---
+++ js/view/foodGridView.js
@@ -0,0 +1,9 @@
+//ExampleView Object constructor
+var FoodGridView = function (container, model) {
+	// Get all the relevant elements of the view (ones that show data
+  	// and/or ones that responed to interaction)
+	this.select = $('.corseSelect');
+	console.log(this.select.val());
+
+}
+
