19aad021098d55e699d2197dc866dc3d38e87f12
Gustav Bergman <gustav@n180-p211.kthopen.kth.se>
2017-01-30 16:41:34 +0100

    fixat view1 klart, dags att börja på view 2.

--- js/view/foodGridView.js
+++ js/view/foodGridView.js
@@ -2,27 +2,35 @@
 var FoodGridView = function (container, model) {
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
-	var select = $('.corseSelect');
+	var select = $('.courseSelect');
 	var recipesHolder = $('.recipes');
+	var search = $('.search');
 
 	// Get dishes of selected type
-	var populateRecipes = function(select,holder){
+	var populateRecipesSelect = function(select,holder){
+		// Clear the holder first, before we add the correct recipes
 		holder.html('');
-		console.log(select.val().toLowerCase());
 		var dishes = model.getAllDishes(select.val().toLowerCase());
 		for(i in dishes){
 			var dish = dishes[i];
 			holder.append(function(){
-				var content = '<div class="recipe-item">'+dish.name+'</div>';
+				var content = '<a href="view2.html>"><div class="recipe-item">';
+				content += '<div class="recipe-item_image"><img src="images/'+dish.image+'"</div>';
+				content += '<div class="recipe-item_heading">'+dish.name+'</div>';
+				content += '</div></a>';
 				return content;
 			});
 		}
 	}
-	populateRecipes(select,recipesHolder);
-
+	// This should be in the controller
+	// change back to using .this in order for the controller to use acess from outside
+	populateRecipesSelect(select,recipesHolder);
 	select.change(function(event) {
 		// Populate the view with the correct recipes
-		populateRecipes(select,recipesHolder);
+		populateRecipesSelect(select,recipesHolder);
 	});
+
+	var populateRecipesSearch = function(search,holder){
+	}
 }
 
