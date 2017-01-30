f9a5064635443cb3835ee4bb2ec7557280544e7b
Gustav Bergman <gustav@n180-p211.kthopen.kth.se>
2017-01-30 15:59:00 +0100

    asd

--- js/view/foodGridView.js
+++ js/view/foodGridView.js
@@ -2,8 +2,27 @@
 var FoodGridView = function (container, model) {
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
-	this.select = $('.corseSelect');
-	console.log(this.select.val());
+	var select = $('.corseSelect');
+	var recipesHolder = $('.recipes');
 
+	// Get dishes of selected type
+	var populateRecipes = function(select,holder){
+		holder.html('');
+		console.log(select.val().toLowerCase());
+		var dishes = model.getAllDishes(select.val().toLowerCase());
+		for(i in dishes){
+			var dish = dishes[i];
+			holder.append(function(){
+				var content = '<div class="recipe-item">'+dish.name+'</div>';
+				return content;
+			});
+		}
+	}
+	populateRecipes(select,recipesHolder);
+
+	select.change(function(event) {
+		// Populate the view with the correct recipes
+		populateRecipes(select,recipesHolder);
+	});
 }
 
